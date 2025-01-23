920df9148e8530d6a7af4681ac2d42988781dd2e
VanTrung <trungnemo@gmail.com>
2025-01-23 10:53:25 +0700

    Update cachematrix.R
    Tao Van Trung edit the folk and final commit it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## By  : Tao Van Trung , email trungnemo@gmail.com 
+## Date: 2025 Jan 23
+## ToDo: Complete the R Programming Assignment 2
 
-## Write a short comment describing this function
-
+##  makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# Creates a matrix that can cache it's inverse
+#
+# Args:
+#   x: A matrix (Optional), if no argument, then a empty matrix is created by default
+#
+# Returns:
+#   A matrix with functions to get/set value & get/set inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  # im cached inverse of matrix
+  im <- NULL
+  # getter for matrix
+  get <- function() x      
+  #setter for matrix
+  set <- function(y){
+    x <<- y
+    im <<- NULL
+  } 
+  #Set the inverse of the matrix 
+  setInverse <- function(solve) im <<- solve
+  getInverse <- function() im
+  list(set=set, get=get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##   cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##       If the inverse has already been calculated (and the matrix has not changed), 
+##       then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(x=matrix(), ...) {
+  # check the inversed matrix
+  im <- x$getInverse()
+  # if im is not null then matrix has been cached/inversed already, return cached data
+  if(!is.null(im)){
+    message("getting cached data")
+    return(im)
+  }
+  #if not, get the matrix and inverse values
+  matrix <- x$get()
+  im <- solve(matrix, ...)
+  x$setInverse(im) #cache it
+  # Return an inversed matrix of 'x'
+  im 
 }
